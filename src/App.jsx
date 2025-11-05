a7daf7814c93f0621747b79f9be96c2ea9f5db1a
aysefat9 <aysefat9@gmail.com>
2025-11-05 22:28:07 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { LoadingScreen } from './components/LoadingScreen'
+import './App.css'
+import "./index.css"
+import {Navbar }from "./components/Navbar"
+import { MobileMenu } from './components/MobileMenu'
+import { Home } from './components/sections/Home'
+import { useState } from 'react'
+
+function App() {
+  const [isLoaded,setisLoaded]=useState(false);
+  const [menuOpen,setMenuOpen]=useState(false)
+  return (
+    <>
+        {!isLoaded &&  <LoadingScreen onComplete={()=>setisLoaded(true)}/>}
+          <div className={`h-full  transition-opacity duration-700 ${isLoaded ? "opacity-100": "opacity-0"} bg-black text-gray-100`}>
+            <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+            <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+            <Home/>
+          </div>
+          
+      </>
+  )
+}
+
+export default App
